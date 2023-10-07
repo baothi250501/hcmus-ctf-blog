6d8e2fdaeabd22b0a373c7c1bc155c95623a78f5
PhuongUyen0712 <75712660+PhuongUyen0712@users.noreply.github.com>
2023-10-07 22:05:13 +0700

    feat: newsfeed UI

--- src/modules/home/HomePage.tsx
+++ src/modules/home/HomePage.tsx
@@ -1,18 +1,31 @@
-import { HomeProps } from "@/pages/index";
-import { useTranslation } from "next-i18next";
-import { homeConfig } from "./homeConfig";
-import useIsMobile from "@/hooks/useIsMobile";
-import { useEffect } from "react";
-import Container from "@/components/layouts/Container";
-import Stack from "@mui/material/Stack";
-import Pagination from "@mui/material/Pagination";
-import { PaginationItem } from "@mui/material";
-export default function HomePage({isSsrMobile}: HomeProps) {
-    const { t } = useTranslation(homeConfig.i18nNamespaces);
-    const isMobile = useIsMobile();
-    return <Container>
-        <main>
-        {courses && courses.data.length > 0 ? (
+import { HomeProps } from '@/pages/index';
+import { useTranslation } from 'next-i18next';
+import { homeConfig } from './homeConfig';
+import useIsMobile from '@/hooks/useIsMobile';
+import { useEffect } from 'react';
+import Container from '@/components/layouts/Container';
+import Stack from '@mui/material/Stack';
+import Pagination from '@mui/material/Pagination';
+import { PaginationItem } from '@mui/material';
+import CurvedNavBar from '@/components/bottomNavBar/PostViewer';
+import PostViewer from '@/components/bottomNavBar/PostViewer';
+export default function HomePage({ isSsrMobile }: HomeProps) {
+  const comments = [
+    { username: 'Thi Nguyễn', content: 'Wow, sự kiện này hay quá!' },
+    { username: 'Vi Nguyễn', content: 'Phải tham gia thôi!' },
+  ];
+  const { t } = useTranslation(homeConfig.i18nNamespaces);
+  const isMobile = useIsMobile();
+  return (
+    <Container>
+      {/* <CurvedNavBar/> */}
+      <PostViewer
+        username="Uyên Lê"
+        avatar="https://via.placeholder.com/50x50"
+        comments={comments}
+      />
+      <main>
+        {/* {courses && courses.data.length > 0 ? (
           <>
             {courses.meta.pagination.pageCount > 1 ? (
               <Stack spacing={2} mt={4} alignItems="center">
@@ -32,7 +45,8 @@ export default function HomePage({isSsrMobile}: HomeProps) {
               </Stack>
             ) : null}
           </>
-        ) : null}
-        </main>
+        ) : null} */}
+      </main>
     </Container>
+  );
 }
